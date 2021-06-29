0b9c385a0696ab2b1628486cb22b75c6b2b8bde8
Fabio Cezar Baía <baiacfabio@gmail.com>
2021-06-29 03:05:01 -0300

    adicionando controller para retornar valor fixo da unidade de metro

---
+++ api_1/WebApi/Controllers/ValorUnidadeMedidaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ValorUnidadeMedidaController : ControllerBase
+    {
+        [HttpGet("{unidadeMedida}")]
+        public ActionResult<decimal> Get(UnidadeMedida unidadeMedida)
+        {
+            return 100m;
+        }
+    }
+
+    public enum UnidadeMedida
+    {
+        Metro = 1,
+        Pe = 2
+    }
+}
